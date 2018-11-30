9bfdd9c145ede66564383816fb785c8d99f626bf
joul <joulomic@hotmail.com>
2018-11-30 22:11:23 +0100

    brand new repo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+     require('dotenv').config({ path: 'variables.env' });
+const express = require('express');
+    const bodyParser = require('body-parser');
+
+    const app = express();
+   const verifyWebhook = require('./verify-webhook');
+    const messageWebhook = require('./message-webhook');
+
+    app.get('/', verifyWebhook);
+    app.post('/', messageWebhook);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(5000, () => console.log('Express server is listening on port 5000'));
