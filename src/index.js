73939481c89d1fd74f3ecb4247d9bf08d0a77109
joul <joulomic@hotmail.com>
2018-11-30 23:14:08 +0100

    port

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ const express = require('express');
     const app = express();
    const verifyWebhook = require('./verify-webhook');
     const messageWebhook = require('./message-webhook');
+const PORT = process.env.PORT || 5000;
 
     app.get('/', verifyWebhook);
     app.post('/', messageWebhook);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(5000, () => console.log('Express server is listening on port 5000'));
+    app.listen(PORT, () => console.log('Express server is listening on port 5000'));
