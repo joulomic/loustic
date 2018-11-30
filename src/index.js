e0a29775a4daf76bb33f8ba0d338169641f56105
joul <joulomic@hotmail.com>
2018-11-30 23:17:45 +0100

    port

--- src/index.js
+++ src/index.js
@@ -12,4 +12,6 @@ const PORT = process.env.PORT || 5000;
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.listen(PORT, () => console.log('Express server is listening on port 5000'));
+    app.listen(PORT, () => {
+      console.log('Express server is listening on port 5000');
+});
