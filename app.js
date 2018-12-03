03429337190d52a95c8701ec7386fa293837d169
joul <joulomic@hotmail.com>
2018-12-03 20:15:54 +0100

    videos

--- app.js
+++ app.js
@@ -54,7 +54,8 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
 
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    //body.entry.forEach(function(entry) {
+    body.entry.forEach((entry) => {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
