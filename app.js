ac0ebaa37ac5ecd4c5e70222571e82f06fbb5ff9
joul <joulomic@hotmail.com>
2018-12-03 20:03:06 +0100

    videos

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.post('/webhook', (req, res) => {
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
+      console.log("trace:");
       console.log(webhook_event);
       processPostback(webhook_event);
     });
