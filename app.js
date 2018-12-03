2ed00fbeb81b1f86e6ad806803310fabdca6f998
joul <joulomic@hotmail.com>
2018-12-03 01:38:58 +0100

    videos

--- app.js
+++ app.js
@@ -23,24 +23,52 @@ app.get("/webhook", function (req, res) {
     res.sendStatus(403);
   }
 });
+
 // All callbacks for Messenger will be POST-ed here
-app.post("/webhook", function (req, res) {
+//app.post("/webhook", function (req, res) {
   // Make sure this is a page subscription
-  if (req.body.object == "page") {
+//  if (req.body.object == "page") {
     // Iterate over each entry
     // There may be multiple entries if batched
-    console.log(req.body.entry);
-    req.body.entry.forEach(function(entry) {
+//    console.log(req.body.entry);
+//    req.body.entry.forEach(function(entry) {
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        }
-      });
+//      entry.messaging.forEach(function(event) {
+//        if (event.postback) {
+//          processPostback(event);
+//        }
+//      });
+//    });
+//
+//    res.sendStatus(200);
+//  }
+//});
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      processPostback(webhook_event);
     });
 
-    res.sendStatus(200);
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
+
 });
 
 function processPostback(event) {
