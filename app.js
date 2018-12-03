93b2c63ea83013a59da85dac3048c9eedd223666
joul <joulomic@hotmail.com>
2018-12-03 22:33:34 +0100

    videos

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function sendMessage(event) {
 
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: PAGE_ACCESS_TOKEN},
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
     method: 'POST',
     json: {
       recipient: {id: sender},
