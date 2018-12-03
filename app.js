4d6a632aaef8a1cb8049cb614bb700d6958256a7
joul <joulomic@hotmail.com>
2018-12-03 21:03:59 +0100

    videos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 
 var app = express();
 //app.use(bodyParser.json());
-app.use(require('connect').bodyParser());
+app.use(require("connect").bodyParser());
 app.use(bodyParser.urlencoded({extended: false}));
 app.listen((process.env.PORT || 5000));
 
